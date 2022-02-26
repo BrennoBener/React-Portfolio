1ef543f0a1dabb6f00c9248c02161d0946200709
BrennoBener <brennobbener@live.com>
2022-02-26 10:26:27 -0300

    algum erro com arquivos npm e tys

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const MainContainer = styled.div`
-background: &{props => props.theme.body};
+background: &{props => props.theme.text};
 width: 100vw;
 height: 100vh;
 `
